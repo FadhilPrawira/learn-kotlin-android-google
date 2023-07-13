6b28cd0def29bd2d98c2051924e218fca6586c51
Fadhil Prawira <fadhilprawira87@gmail.com>
2023-07-13 14:15:06 +0700

    Create App.kt

---
+++ [01] Android Basics with Compose/[03] Create and use functions in Kotlin/App.kt
@@ -0,0 +1,8 @@
+fun main() {
+    birthdayGreeting()
+}
+
+fun birthdayGreeting(): Unit { // Unit same as void in java
+    println("Happy Birthday, Rover!")
+    println("You are now 5 years old!")
+}
